8cc113e1f6ceab6b2ae76db79dbb6d0c9e32cd02
Kyoungsue Kim <zp5njqlfex@gmail.com>
2024-02-02 18:53:06 +0900

    fix: fix http2 problems on windows.

--- sturdy-chainsaw/logger.py
+++ sturdy-chainsaw/logger.py
@@ -4,9 +4,13 @@ from datetime import datetime
 
 class Config:
     log_file_name = f'logs/logs_{datetime.now().strftime("%Y-%m-%d %I-%M-%S")}.txt'
+    dir_name = os.path.dirname(log_file_name)
 
 
 def logger(text):
+    if not os.path.exists(Config.dir_name):
+        os.makedirs(Config.dir_name, exist_ok=True)
+
     with open(Config.log_file_name, 'w') as file:
         print(f"[{datetime.now().strftime('%Y/%m/%d %I:%M:%S')}] {text}" if text != "\n" else "\n", file=sys.stderr, flush=True)
 
